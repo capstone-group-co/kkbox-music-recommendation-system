26daea5de6f4a204e0a9c238b4dcdbd10424870f
tkelduzdar <tkelduzdar@gwmail.gwu.edu>
2017-10-12 17:37:17 -0400

    Explored and Adjusted data types
    Explored and Adjusted data types for tables Members, Songs,
    Song_Extra_Info and Train
    
    Thought of new variable which is score for each song, still working on
    improving it

--- code_import.py
+++ code_import.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[11]:
+# In[2]:
 
 
 # Import the `pandas` library as `pd`
@@ -19,42 +19,42 @@ print(list(members))
 
 
 
-# In[12]:
+# In[3]:
 
 
 # Number of Rows and Columns
 print(members.shape)
 
 
-# In[13]:
+# In[5]:
 
 
 # Summary Statistics of Columns
 print(members.describe())
 
 
-# In[14]:
+# In[6]:
 
 
 # Number of Memebrs
 print("{:,} Members".format(members.shape[0]))
 
 
-# In[15]:
+# In[7]:
 
 
 # Number of null values in each columns
 print(members.isnull().sum())
 
 
-# In[16]:
+# In[8]:
 
 
 # Variable Types for Members Table
 print(members.dtypes)
 
 
-# In[17]:
+# In[9]:
 
 
 ###### ADJUST COLUMN TYPES
@@ -68,33 +68,24 @@ print(members.head)
 
 
 
-# In[18]:
+# In[10]:
 
 
 # Variable Types for `Members Table
 print(members.dtypes)
 
 
-# In[19]:
+# In[48]:
 
 
-# Change City and Registred Via Columns into type Object
-members['city'] = members['city'].astype(object)
-members['registered_via'] = members['registered_via'].astype(object)
+# Change City - Registred Via - Gender Columns into type Object
+members['city'] = members['city'].astype('category')
+members['registered_via'] = members['registered_via'].astype('category')
+members['gender'] = members['gender'].astype('category')
 print(members.dtypes)
 
 
-# In[20]:
-
-
-import matplotlib.pyplot as plt
-
-plt.hist(members['city'],bins=21)  # arguments are passed to np.histogram
-plt.title("Histogram with 'auto' bins")
-plt.show()
-
-
-# In[72]:
+# In[49]:
 
 
 # Explore the Songs Table
@@ -104,7 +95,7 @@ songs = pd.read_csv("C:/Data/Fall 2017/Capstone/raw_data/songs.csv")
 
 
 
-# In[73]:
+# In[50]:
 
 
 # Name of columns
@@ -115,36 +106,186 @@ print("Rows","Columns",
     members.shape,)
 
 
-# In[74]:
+# In[70]:
 
 
 # Sample of Songs table
-print(songs.head())
+print(songs.head(3))
 
 
-# In[82]:
+# In[52]:
 
 
 # Variable Types for `Songs Table
 print(songs.dtypes)
 
 
-# In[81]:
+# In[53]:
 
 
 print(songs.describe())
 
 
-# In[117]:
+# In[54]:
 
 
-# TRYING TO CHANGE THE SONG_LENGTH TO Minutes and Seconds (Still under process)
-
+#change Language to Object type without the decimals
 import numpy as np
-sec=np.array(songs["song_length"])
-sec=sec/60
-sec=sec/60
-print(sec.mean())
-print(sec.max())
+songs['language']=np.nan_to_num(songs['language']).astype(int)
+songs['language']=songs.language.astype('category')
 
+
+# In[55]:
+
+
+# What are the Language codes available
+songs.language.unique()
+
+
+# In[57]:
+
+
+# Variable Types for `Songs Table
+print(songs.dtypes)
+
+
+# In[60]:
+
+
+# Change Song_Length to Float type
+songs['song_length']= songs.song_length.astype(float)
+
+
+# In[62]:
+
+
+# Variable Types for `Songs Table
+print(songs.dtypes)
+
+
+# In[64]:
+
+
+# Number of null values in each columns
+print(songs.isnull().sum())
+
+
+# In[66]:
+
+
+# Explore the song_extra_info Table
+
+# Load in the data with `read_csv()`
+song_extra_info = pd.read_csv("C:/Data/Fall 2017/Capstone/raw_data/song_extra_info.csv")
+
+
+# In[68]:
+
+
+# Name of columns
+print(list(song_extra_info))
+
+# Number of Rows and Columns
+print("Rows","Columns",
+    song_extra_info.shape,)
+
+
+# In[71]:
+
+
+# Variable Types for `Song_Extra_Info Table
+print(song_extra_info.dtypes)
+
+
+# In[74]:
+
+
+# Sample of Song_Extra_Info table
+print(song_extra_info.head(3))
+
+
+# In[210]:
+
+
+# Explore the train dataset
+
+# Load in the train dataset with `read_csv()`
+train = pd.read_csv("C:/Data/Fall 2017/Capstone/raw_data/train.csv")
+
+
+# In[211]:
+
+
+# Name of columns
+print(list(train))
+
+# Number of Rows and Columns
+print("Rows","Columns",
+    train.shape,)
+
+# Variable Types for train dataset
+print(train.dtypes)
+
+
+# In[212]:
+
+
+# Change target to Category data type
+train['target']= train.target.astype('category')
+
+
+# In[213]:
+
+
+# Sample of train dataset
+print(train.head(3))
+
+
+# In[214]:
+
+
+# Distribution of training dataset for target 
+train.target.value_counts()/len(train.target) *100
+
+
+# In[215]:
+
+
+# Number of null values in each columns
+print(train.isnull().sum())
+
+
+# In[216]:
+
+
+# Number of Users in train dataset
+print(len(train.msno.unique()),"Users in the train dataset") 
+
+
+# In[263]:
+
+
+######  Song Score Varibale  
+
+#Create a Songs Score table copy of song id and target
+songs_score= train[['song_id','target']].copy(deep=True)
+
+#Rename Target column to Score
+songs_score.columns=['song_id','score']
+
+#Change Score type to float 
+songs_score['score']=songs_score.score.astype(float)
+
+#Replace all target 0 with -1 
+songs_score.score=songs_score.score.replace(0,-1)
+
+# Group by song id and sum scores column in order to have final score for each song
+songs_score=songs_score.groupby('song_id',as_index=False).sum()
+
+
+# In[274]:
+
+
+# From this Score table we can check if the song is liked by many users or not (STILL UNDER CONSTRUCTION, I AM THINKING OF ALGORITHM) 
+songs_score.describe()
 
