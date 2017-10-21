85b70057fa74dd1356ec7af2ce28ad7286ef7911
Chuanye "Elvin" Ouyang <elvin.ouyang@gmail.com>
2017-10-21 12:40:18 -0400

    create standard cleaning module
    standardized_import will be used as data import pipeline

--- elvin_eda_codes.py
+++ elvin_eda_codes.py
@@ -3,31 +3,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
-# Load files and report basic information about dataframes
-
-file_names = ['raw_data/members.csv',
-              'raw_data/song_extra_info.csv',
-              'raw_data/songs.csv',
-              'raw_data/test.csv',
-              'raw_data/train.csv']
-
-members, song_extra_info, songs, test, train = [pd.read_csv(x)
-                                                for x in file_names]
-
-files = [members, song_extra_info, songs, test, train]
-
 
 def describe_df(df, df_name):
-    var_names = " ".join(df.columns.values.astype('U'))
+    dtypes = df.dtypes.to_frame(name='dtypes').T
+    n_null = df.isnull().sum()[df.isnull().sum() != 0].\
+        to_frame(name='nulls').T
     print("""
-The dataframe {} has {} columns/variables and {} observations.
-The dataframe comes with the following variables:
+$$$ The dataframe {} has {} columns/variables and {} observations.
+    The dataframe comes with the following variables:
+{}
+    The following variables have null values:
 {}
     """.format(
-        df_name, df.shape[1], df.shape[0], var_names), end='')
+        df_name, df.shape[1], df.shape[0], dtypes, n_null), end='')
 
-for x, y in zip(files, file_names):
-    describe_df(x, y)
+
+# Load files and report basic information about dataframes
+if __name__ == "__main__":
+    file_names = ['raw_data/members.csv',
+                  'raw_data/song_extra_info.csv',
+                  'raw_data/songs.csv',
+                  'raw_data/test.csv',
+                  'raw_data/train.csv']
+
+    members, song_extra_info, songs, test, train = [pd.read_csv(x)
+                                                    for x in file_names]
+
+    files = [members, song_extra_info, songs, test, train]
+
+    for x, y in zip(files, file_names):
+        describe_df(x, y)
 
 # DATA CLEANING STEPS
 
