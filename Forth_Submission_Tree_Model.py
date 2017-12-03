783a223b210f9067cacaf595a878520b20e4344e
tkelduzdar <tkelduzdar@gwmail.gwu.edu>
2017-12-03 18:29:51 -0500

    Forth Submission using Decision Tree model
    This is the code that i ran on EC2 instance. It applies the Decision
    Tree algorithm on the full_train and full_test datasets.
    Note: Model was trained on full train dataset without splitting to train
    and test because the target value in train is different than the one we
    submit (Binary vs Float).

---
+++ Forth_Submission_Tree_Model.py
@@ -0,0 +1,101 @@
+
+# coding: utf-8
+
+# In[24]:
+
+
+# Since the full_train and full_test datasets were very big in size exceeding 16 gb in total
+# We made use of cloud computing through using AWS S3 and EC2 :
+# Datasets were uploaded on AWS S3 bucket
+# Using an EC2 instance with 256 GB Ram we have used the following code :
+
+# Import boto3 and io libraries to connect to Amazon S3 
+import boto3
+import io
+import pandas as pd
+import numpy as np
+
+# Connect to the AWS S3 service
+s3_client = boto3.client('s3')
+
+# Connect to the S3 bucket and select the file to import
+response = s3_client.get_object(Bucket="www.g43865368.gwu.edu",Key="full_train.csv")
+
+# Save the full_train file as a Pandas Dataframe 
+file = response["Body"].read()
+train=pd.read_csv(io.BytesIO(file), delimiter=",")
+
+
+# Free some memory by deleting unused variables
+del response
+del file
+
+
+# Fill Nan values with 0
+train=train.fillna(0)
+
+
+# Assign Features and Target Columns to variables 'features' and 'target' to be used in training the model 
+features = list(train.values[:,4:])
+target = list(train.values[:,3])
+
+
+# Import decision tree 'tree' algorithm from sklearn library
+from sklearn import tree
+
+# Assign the decision tree classifier to variable 'clf'
+clf = tree.DecisionTreeClassifier()
+
+
+# Train the Decison Tree model according to training dataset Features and Targets
+clf = clf.fit(features, target)
+
+
+# Free some memory deleting the train dataset as it won't be used anymore 
+del train
+
+
+#Import the test dataset that will be used for submission of the predictions
+
+# Connect to the AWS S3 service
+s3_client = boto3.client('s3')
+
+# Connect to the S3 bucket and select the file to import
+response = s3_client.get_object(Bucket="www.g43865368.gwu.edu",Key="full_test.csv")
+
+# Save the full_test file as a Pandas Dataframe 
+file = response["Body"].read()
+test=pd.read_csv(io.BytesIO(file), delimiter=",")
+
+
+# Fill Nan values with 0
+test=test.fillna(0)
+
+
+# Assign Features Columns from Test dataset to variables 'features' to be used in predicting the targets 
+features = list(test.values[:,4:])
+
+
+# Predict Target Probability for the test dataset
+target_pred = clf.predict_proba(features)
+
+
+# Create a for loop to predict each row of the final test and save it to final_pred dataframe
+final_pred=[]
+for i in (list(range(len(test)))):
+    #print(i)
+    test_id = str(test.id.iloc[i])
+
+    # Because the predict_proba gives as array for the probability for each class 0 and 1 in our case. We will only use the
+    # Probability of class 1 which is the second element of the array
+
+    predicted_rating = target_pred[i][1]
+    
+    final_pred.append([test_id,predicted_rating])
+    
+final_pred=pd.DataFrame(final_pred)
+final_pred.columns=['id','target']
+
+# Save the final_pred to CSV to submit
+final_pred.to_csv('forth_submission.csv',index=False, float_format = '%.5f')
+
