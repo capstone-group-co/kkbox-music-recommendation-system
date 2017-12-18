1ccbbf6d043896cfa4172956133d0b6751726438
Chuanye "Elvin" Ouyang <elvin.ouyang@gmail.com>
2017-12-18 09:08:42 -0500

    grid search best results for light GBM

--- main_lgbm_training_cleaned_to_params_grid_search.py
+++ main_lgbm_training_cleaned_to_params_grid_search.py
@@ -1,5 +1,15 @@
 """
-generating predictions based on raw csv input files from end to end
+generating predictions based on clean csv input and sklearn GridSearchCV with
+hyperparameters for Light GBM.
+Optimal parameters as of now:
+num_leaves = 256
+max_depths = 9
+learning_rate = 0.3
+subsamples = 1
+
+to test:
+reg_alphas = [0, 0.05, 0.1]
+reg_lambdas = [0, 0.05, 0.1]
 """
 
 import pandas as pd
@@ -10,9 +20,10 @@ from sklearn.model_selection import GridSearchCV
 import lightgbm as lgb
 
 print(">>> Defining hyper parameters")
-max_depths = [8, 9]
+learning_rate = 0.3
 num_leaves = 256
-learning_rates = [0.2, 0.8]
+max_depth = 9
+subsamples = [1, 0.9, 0.8, 0.7]
 
 print(">>> Preparing data for lgbm training...")
 train = pd.read_csv("../raw_data/full_train.csv")
@@ -34,25 +45,22 @@ del train, test
 gc.collect()
 
 print('>>> Training LGBM model...')
-estimator = lgb.LGBMClassifier(object='binary',
+estimator = lgb.LGBMClassifier(objective='binary',
                                num_leaves=num_leaves,
+                               learning_rate=learning_rate,
                                random_state=1122,
-                               n_jobs=2,
+                               max_depth=max_depth,
+                               n_jobs=5,
                                n_estimators=100)
 
 param_grid = {
-    'learning_rate': learning_rates,
-    'max_depth': max_depths
+    'subsample': subsamples
 }
 
 gbm = GridSearchCV(estimator, param_grid, verbose=1)
 gbm.fit(X, y)
 best_params = gbm.best_params_
 print(">>> Best parameters: %s" % best_params)
-
-print(">>> Feature Importances:")
-print(X.columns)
-print(gbm.feature_importances_)
 
 print('>>> Making predictions and saving them...')
 p_test = gbm.predict(X_test)
